fd6b8203fe13be0bc5ee7ca5c8a4246f832aff73
Joo-nam Kim <joo-namkim@Joo-nams-MacBook-Pro.local>
2020-03-30 19:56:13 +0900

    pagination

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ApiService} from '../api.service';
+import {HttpResponse} from '@angular/common/http';
+import {takeUntil} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 
 @Component({
@@ -10,11 +13,52 @@ import {ApiService} from '../api.service';
 export class HomeComponent implements OnInit {
 
   products = [];
+  private destroy$: Observable<any> = new Observable();
   constructor(private apiService: ApiService) {}
-  ngOnInit() {
-    this.apiService.sendGetRequest().subscribe((data: any[]) => {
-      console.log(data);
-      this.products = data;
+  // ngOnInit() {
+  //   this.apiService.sendGetRequest().subscribe((data: any[]) => {
+  //     console.log(data);
+  //     this.products = data;
+  //   });
+  // }
+  ngOnInit(){
+    this.apiService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>)=>{
+      console.log(res);
+      this.products = res.body;
+    });
+  }
+  public firstPage() {
+    this.products = [];
+    this.apiService.sendGetRequestToUrl(this.apiService.first).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
+    });
+  }
+  public previousPage() {
+
+    if (this.apiService.prev !== undefined && this.apiService.prev !== '') {
+      this.products = [];
+      this.apiService.sendGetRequestToUrl(this.apiService.prev).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+    }
+
+  }
+  public nextPage() {
+    if (this.apiService.next !== undefined && this.apiService.next !== '') {
+      this.products = [];
+      this.apiService.sendGetRequestToUrl(this.apiService.next).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+        console.log(res);
+        this.products = res.body;
+      });
+    }
+  }
+  public lastPage() {
+    this.products = [];
+    this.apiService.sendGetRequestToUrl(this.apiService.last).pipe(takeUntil(this.destroy$)).subscribe((res: HttpResponse<any>) => {
+      console.log(res);
+      this.products = res.body;
     });
   }
 }
